4c5bed44894f8d996583c98f39d487819445018e
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-07-31 17:57:00 +0300

    create Rental entity

---
+++ src/main/java/mate/academy/car/sharing/entity/Rental.java
@@ -0,0 +1,26 @@
+package mate.academy.car.sharing.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Entity
+@Accessors(chain = true)
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate rentalDate;
+    private LocalDate returnDate;
+    private LocalDate actualReturnDate;
+    private Long carId;
+    private Long userId;
+}
