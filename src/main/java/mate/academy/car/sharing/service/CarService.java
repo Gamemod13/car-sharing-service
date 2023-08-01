f68b1a8388d9550a65ea87f564dfd0e0881d54f6
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-08-01 12:06:50 +0300

    create AbstractService and Service + Repository layer for Car entity

--- src/main/java/mate/academy/car/sharing/service/CarService.java
+++ src/main/java/mate/academy/car/sharing/service/CarService.java
@@ -1,16 +1,6 @@
 package mate.academy.car.sharing.service;
 
-import java.util.List;
 import mate.academy.car.sharing.entity.Car;
 
-public interface CarService {
-    Car add(Car car);
-
-    Car getById(Long id);
-
-    List<Car> getAll();
-
-    Car update (Car car);
-
-    void delete(Long id);
+public interface CarService extends AbstractService<Car>  {
 }
