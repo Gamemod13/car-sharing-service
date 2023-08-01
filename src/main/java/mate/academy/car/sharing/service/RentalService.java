1f162d0130adac486d1e3dcb1890a1bd0fa37a58
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-08-01 16:25:51 +0300

    create rental repository and service

---
+++ src/main/java/mate/academy/car/sharing/service/RentalService.java
@@ -0,0 +1,6 @@
+package mate.academy.car.sharing.service;
+
+import mate.academy.car.sharing.entity.Rental;
+
+public interface RentalService extends AbstractService<Rental> {
+}
---
+++ src/main/java/mate/academy/car/sharing/service/impl/RentalServiceImpl.java
@@ -0,0 +1,34 @@
+package mate.academy.car.sharing.service.impl;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.car.sharing.entity.Rental;
+import mate.academy.car.sharing.repository.RentalRepository;
+import mate.academy.car.sharing.service.RentalService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RentalServiceImpl implements RentalService {
+    private final RentalRepository rentalRepository;
+
+    @Override
+    public Rental add(Rental rental) {
+        return rentalRepository.save(rental);
+    }
+
+    @Override
+    public Rental getById(Long id) {
+        return rentalRepository.getReferenceById(id);
+    }
+
+    @Override
+    public List<Rental> getAll() {
+        return rentalRepository.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        rentalRepository.deleteById(id);
+    }
+}
