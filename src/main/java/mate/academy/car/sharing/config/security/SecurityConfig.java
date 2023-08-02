ad33fc51f5d9098d9d71b1e95fbd3062ea2bb094
Trach Vika <vikaa@Oksanas-MacBook-Pro.local>
2023-08-02 13:54:07 +0300

    create swagger

--- src/main/java/mate/academy/car/sharing/config/security/SecurityConfig.java
+++ src/main/java/mate/academy/car/sharing/config/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package mate.academy.car.sharing.config.security;
 
 import lombok.RequiredArgsConstructor;
+import mate.academy.car.sharing.entity.User;
 import mate.academy.car.sharing.security.jwt.JwtConfigurer;
+import mate.academy.car.sharing.security.jwt.JwtTokenFilter;
 import mate.academy.car.sharing.security.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,20 +31,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().permitAll()
-//                .and()
-//                .apply(new JwtConfigurer(jwtTokenProvider))
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .csrf().disable();
-        http.httpBasic().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers().permitAll()
+                .antMatchers("/register", "/inject", "/login", "/health", "/v2/api-docs",
+                    "/swagger-resources/**", "/swagger-ui.html", "/webjars/**")
+                .permitAll()
+                .antMatchers(HttpMethod.DELETE, "/cars/*", "/users/*")
+                .hasRole(User.Role.MANAGER.name())
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+
+        http.addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
+
 
     @Bean
     public PasswordEncoder getEncoder() {
--- src/main/java/mate/academy/car/sharing/config/swagger/SwaggerConfig.java
+++ src/main/java/mate/academy/car/sharing/config/swagger/SwaggerConfig.java
@@ -3,6 +3,7 @@ package mate.academy.car.sharing.config.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -19,7 +20,7 @@ public class SwaggerConfig {
     public Docket scrumAllyApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(regex("/"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
