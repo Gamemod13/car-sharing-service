1e2257cfc7c061610fb09aadd93e76e453b830bf
Gamemod13 <igorfetov1997@gmail.com>
2023-08-02 09:50:59 +0300

    Removed password from UserResponseDto, using @Data

--- src/main/java/mate/academy/car/sharing/dto/response/UserResponseDto.java
+++ src/main/java/mate/academy/car/sharing/dto/response/UserResponseDto.java
@@ -1,14 +1,14 @@
 package mate.academy.car.sharing.dto.response;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
-@Getter
-@Setter
+@Data
 public class UserResponseDto {
     private String email;
     private String firstName;
     private String lastName;
-    private String password;
     private String role;
 }
