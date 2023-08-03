b782d24cb6b1ff5f3f618a5c6e47abad001a7534
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-08-03 13:40:59 +0300

    added password validation and added annotations to UserRegistrationDto

--- src/main/java/mate/academy/car/sharing/dto/request/UserRegistrationDto.java
+++ src/main/java/mate/academy/car/sharing/dto/request/UserRegistrationDto.java
@@ -1,9 +1,13 @@
 package mate.academy.car.sharing.dto.request;
 
+import javax.validation.constraints.Email;
 import lombok.Data;
+import mate.academy.car.sharing.validation.Password;
 
+@Password
 @Data
 public class UserRegistrationDto {
+    @Email
     private String email;
     private String password;
     private String repeatPassword;
