eef420981123dde3a17cc57f310321aa64851a75
Gamemod13 <igorfetov1997@gmail.com>
2023-08-03 15:48:07 +0300

    Implement TelegramNotificationService + ScheduledTelegramNotificationServiceImpl

--- src/main/java/mate/academy/car/sharing/service/impl/TelegramNotificationServiceImpl.java
+++ src/main/java/mate/academy/car/sharing/service/impl/TelegramNotificationServiceImpl.java
@@ -1,10 +1,6 @@
 package mate.academy.car.sharing.service.impl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import javax.annotation.PostConstruct;
-import lombok.Getter;
 import mate.academy.car.sharing.exception.TelegramNotificationException;
 import mate.academy.car.sharing.service.TelegramNotificationService;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,23 +9,29 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
-import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScope;
-import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeChat;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@Getter
 @Service
 @PropertySource("classpath:application.properties")
 public class TelegramNotificationServiceImpl extends TelegramNotificationService {
-    private static final String STARTED = "Started";
+    private static final String ADMIN_CHAT_ID = "-1001600483081";
     @Value("${telegram.bot.username}")
     private String botUsername;
     @Value("${telegram.bot.token}")
     private String botToken;
     @Value("${telegram.chat.id}")
     private String adminChatId;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
 
     @Override
     public void sendMessage(String chatId, String message) {
@@ -45,12 +47,21 @@ public class TelegramNotificationServiceImpl extends TelegramNotificationService
     }
 
     @Override
+    public void sendMessageToAdminChat(String message) {
+        sendMessage(ADMIN_CHAT_ID, message);
+    }
+
+    @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             String chatId = String.valueOf(update.getMessage().getChatId());
             if (messageText.equals("/start")) {
-                sendMessage(chatId, STARTED);
+                System.out.println(chatId);
+                sendMessage(chatId, "Hi, " + update.getMessage().getFrom().getFirstName()
+                        + ", bot "
+                        + "has "
+                        + "been started");
             }
         }
     }
@@ -64,14 +75,5 @@ public class TelegramNotificationServiceImpl extends TelegramNotificationService
         } catch (TelegramApiException e) {
             throw new TelegramNotificationException("Can't register bot");
         }
-    }
-
-    private void setCommandScopes(){
-        List<BotCommand> commands = List.of(
-                new BotCommand("",""),
-                new BotCommand("",""),
-                new BotCommand("",""),
-                new BotCommand("","")
-        );
     }
 }
