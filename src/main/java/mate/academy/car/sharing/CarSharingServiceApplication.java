b4a47e5127f983baec4e03bdaaf8b358324c73fb
Gamemod13 <igorfetov1997@gmail.com>
2023-08-03 16:11:35 +0300

    Fix SpringApp

--- src/main/java/mate/academy/car/sharing/CarSharingServiceApplication.java
+++ src/main/java/mate/academy/car/sharing/CarSharingServiceApplication.java
@@ -1,9 +1,7 @@
 package mate.academy.car.sharing;
 
-import mate.academy.car.sharing.service.TelegramNotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -11,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CarSharingServiceApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context =
-                SpringApplication.run(CarSharingServiceApplication.class, args);
+        SpringApplication.run(CarSharingServiceApplication.class, args);
     }
 }
